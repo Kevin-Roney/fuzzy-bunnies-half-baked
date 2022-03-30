78021e474e7a6734173cac81b7349945890d856c
Kevin Roney <ktroney.pdx@gmail.com>
2022-03-30 12:14:18 -0700

    added delete bunny function to fetch-utils.js

--- fetch-utils.js
+++ fetch-utils.js
@@ -18,7 +18,10 @@ export async function getFamilies() {
 
 export async function deleteBunny(id) {
     // delete a single bunny using the id argument
-
+    const response = await client
+        .from('fuzzy_bunnies')
+        .delete()
+        .match(id);
     return checkError(response);
 }
 
