8908a0dce1d05609f8a3852a9d0dcd4e8928ab67
Kevin Roney <ktroney.pdx@gmail.com>
2022-03-30 12:38:54 -0700

    seemingly finished functions on create.js, wont know until families.js is finished

--- create/create.js
+++ create/create.js
@@ -7,23 +7,29 @@ import {
 
 const form = document.querySelector('.bunny-form');
 const logoutButton = document.getElementById('logout');
+const selectEl = document.querySelector('select');
 
 form.addEventListener('submit', async e => {
     // prevent default
-
+    e.preventDefault();
     // get the name and family id from the form
-
+    const data = new FormData(form);
     // use createBunny to create a bunny with this name and family id
-    
+    await createBunny(data.get('id'), data.get('name'));
     form.reset();
 });
 
-window.addEventListener('load', async() => {
+window.addEventListener('load', async () => {
     // let's dynamically fill in the families dropdown from supabase
     // grab the select HTML element from the DOM
-
+    const families = await getFamilies();
     // go get the families from supabase
-
+    for (let family of families) {
+        const optionEl = document.createElement('option');
+        optionEl.textContent = family.name;
+        optionEl.value = family.id;
+        selectEl.append(optionEl);
+    }
     // for each family
 
     // create an option tag
